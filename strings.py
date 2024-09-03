349f5dc69e98b00bb6218a6e876e2d8d4c982257
bluumovingon <ariandra502@gmail.com>
2024-09-03 19:22:36 +0800

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+course =  'Strings Excercise'
+print(course.find('c'))
+
+#untuk mencari huruf.
+#angka yang  muncul di terminal adalah letak huruf pertama(0,1,2,3,4,5...).
+
+course = 'Strings excercise'
+print('excercise' in course)
+
+
+course = 'Latihan'
+print(course.replace('Latihan', ('excercise')))
+#untuk mengubah course
