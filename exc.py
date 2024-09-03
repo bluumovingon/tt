f0bb57659588bd4272dfe1aa0a966fa743cc1236
bluumovingon <ariandra502@gmail.com>
2024-09-03 19:13:26 +0800

    Update exc.py

--- exc.py
+++ exc.py
@@ -1,7 +1,8 @@
-weight = float(input("weight "))
-unit = input("(K)g or (L)bs: ")
+weight = int(input("weight "))
+unit = input("(K)gs or (L)bs: ")
 if unit.upper() == "K":
    converted = weight / 0.45
    print("Weight in (L)bs: " + str(converted))
-else: converted = weight * 0.45
-print("Weight in (K)gs: " + str(converted))
+else: 
+   converted = weight * 0.45
+   print("Weight in (K)gs: " + str(converted))
