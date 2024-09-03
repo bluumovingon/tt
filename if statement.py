41861cb0641485160cc85fc24b9a9467f1370ccb
bluumovingon <ariandra502@gmail.com>
2024-09-03 19:30:47 +0800

    Add files via upload

---
+++ if statement.py
@@ -0,0 +1,11 @@
+temperature = 25
+
+if temperature > 30: 
+    print("it's a hot day")
+    print("drink plenty of water")
+elif temperature > 20:
+     print("it's a nice day") 
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("I'ts Cold")
